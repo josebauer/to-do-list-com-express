efdb55358ef10f564ddf6db36cc22bbd21cfb7c9
José Bauer <jose.h.bauer03@gmail.com>
2024-02-06 10:04:14 -0300

    Cria models checklist e task

--- config/database.js
+++ config/database.js
@@ -5,5 +5,5 @@ main()
   .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/to-do-list');
 }
