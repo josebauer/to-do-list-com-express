f388f0c86d9229ec5b3789d0510c376fb7c84834
José Bauer <jose.h.bauer03@gmail.com>
2024-02-12 14:16:15 -0300

    Adiciona método PUT para alterar nome da lista

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -22,15 +22,24 @@ router.get('/new', async (req, res) => {
   }
 })
 
+router.get('/:id/edit', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id)
+    res.status(200).render('checklists/edit', { checklist: checklist })
+  } catch (error) {
+    res.status(500).render('pages/error', { error: 'Erro ao Exibir a edição da lista' })
+  }
+})
+
 router.post('/', async (req, res) => {
   let { name } = req.body.checklist
-  let checklist = new Checklist({name})
+  let checklist = new Checklist({ name })
 
   try {
     await checklist.save()
     res.redirect('/checklists')
   } catch (error) {
-    res.status(422).render('checklists/new', { checklist: { ...checklist, error} })
+    res.status(422).render('checklists/new', { checklist: { ...checklist, error } })
   }
 })
 
@@ -44,13 +53,15 @@ router.get('/:id', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-  let { name } = req.body
+  let { name } = req.body.checklist
+  let checklist = await Checklist.findById(req.params.id)
 
   try {
-    let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true })
-    res.status(200).json(checklist)
+    await checklist.update({ name })
+    res.redirect('/checklists')
   } catch (error) {
-    res.status(422).json(error)
+    let errors = error.errors
+    res.status(422).render('checklists/edit', { checklist: { ...checklist, errors } })
   }
 })
 
